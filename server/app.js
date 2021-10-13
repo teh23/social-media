1174e566bf47a24781fce23cf821974a35d4163e
teh23 <teh23@users.noreply.github.com>
2021-10-13 14:27:00 +0200

    init mongoose and middlewares

--- server/app.js
+++ server/app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const compression = require('compression');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const DB_URL = process.env.DB_URL;
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
 app.use(compression());
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+  res.send('asd');
+});
+
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const app = require('./app');
 
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : '3001';
-app.get('/', (req, res) => {
-  res.send('asd');
-});
 
 app.listen(PORT, () => {
-  console.log('listen');
+  console.log('HTTP SERVER ON');
 });
