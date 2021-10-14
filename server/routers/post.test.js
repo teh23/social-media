f095977afb4833d42be51cc73807d2a1070560e6
teh23 <teh23@users.noreply.github.com>
2021-10-14 09:46:22 +0200

    init post test

---
+++ server/routers/post.test.js
@@ -0,0 +1,33 @@
+import '@babel/polyfill';
+
+const supertest = require('supertest');
+const app = require('../app');
+const { setData } = require('./mock');
+const api = supertest(app);
+const Post = require('../models').Post;
+const { findOneUser } = require('../services').users;
+const { closeMongoose } = require('./mock');
+
+beforeEach(async () => {
+  await setData();
+});
+
+describe('post tests', () => {
+  test('get all posts', async () => {
+    const posts = await api
+      .get('/api/post')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+    expect(posts.body).toBeEqual({
+      title: '',
+      body: '',
+      user: '',
+      media: '',
+      comments: '',
+    });
+  });
+});
+
+afterAll(() => {
+  closeMongoose();
+});
